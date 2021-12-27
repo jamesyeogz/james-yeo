4cf40ebf85bde454308f884d1f7a8e590a622e36
unknown <jamesyeogz@gmail.com>
2021-12-27 21:20:16 +0800

    More Updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,34 +2,34 @@ import React from "react";
 import background_Home from "./img/Home.jpg";
 import background_Case from "./img/Case.jpg";
 import background_Intern from "./img/Office.png";
-
+import { Link  } from "react-router-dom"; 
 const Footer = () => {
   return (
     <div className="section-half">
       
       <div className="footer">
         <div className="footer-content-row">
-          <a href="/james-yeo">
+          <Link to="/james-yeo">
             <div className="background-image" style={{ backgroundImage: `url(${background_Home})` }}></div>
             <h1>Home</h1>
-          </a>
+          </Link>
         </div>
         <div
           className="footer-content-row"
         >
-          <a href="/james-yeo">
+          <Link to="/james-yeo">
           <div className="background-image" style={{ backgroundImage: `url(${background_Case})` }}></div>
           <h1>Case Studies</h1>
-          </a>
+          </Link>
          
         </div>
         <div
           className="footer-content-row"
         >
-          <a href="/james-yeo/Internships">
+          <Link to="/james-yeo/Internships">
           <div className="background-image" style={{ backgroundImage: `url(${background_Intern})` }}></div>
             <h1>Internships</h1>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
