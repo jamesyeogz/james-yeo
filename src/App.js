138edc742b6d0ed0e63405eee018575ae25edb22
unknown <jamesyeogz@gmail.com>
2021-12-27 15:49:05 +0800

    I have no Idea What Am I doing

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/james-yeo" element={<Home />} />
         <Route path="/Internships" element={<Intern />} />
       </Routes> 
     </Router>
